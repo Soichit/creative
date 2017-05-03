a7cfda788a2f4ade3037003eebfa28cb95eae0a9
soichit <soichit@uw.edu>
2017-05-02 22:34:14 -0700

    added maps

--- main.js
+++ main.js
@@ -8,6 +8,8 @@
             projectImgs[i].onmouseover=circleRadius;
             projectImgs[i].onmouseout=squareRadius;
         }
+
+        initMap();
     };
 
     function circleRadius() {
@@ -19,5 +21,34 @@
         //console.log(this);
         this.style.borderRadius = "15%";
     }
+
+
+    function initMap() {        
+        var portland = {company: "SightLine Applications", lat: 45.523, lng: -122.677};
+        var seattle = {company: "Bellhapp", lat: 47.606, lng: -122.332};
+        var austin = {company: "NetSuite", lat: 30.267, lng: -97.743};
+        var cities = [portland, seattle, austin];
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 3,
+          center: seattle
+        });
+
+        cities.forEach(function(city) {
+          //console.log(city);
+          var contentString = "<h2>" + city.company + "</h2>";
+          var infowindow = new google.maps.InfoWindow({
+            content: contentString
+          });
+          var marker = new google.maps.Marker({
+            position: city,
+            map: map,
+            title: city.company
+          });
+          marker.addListener('click', function() {
+            infowindow.open(map, marker);
+          });
+        });
+      }
 
 }());
